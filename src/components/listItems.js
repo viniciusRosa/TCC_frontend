ea44d73edee08228ff9ccebfd55128208f90b51e
vinicius rosa <vinicius.rosa00@gmail.com>
2020-03-06 16:22:21 -0300

    create menu

--- src/components/listItems.js
+++ src/components/listItems.js
@@ -12,67 +12,165 @@ import DirectionsBusIcon from '@material-ui/icons/DirectionsBus';
 import SettingsIcon from '@material-ui/icons/Settings';
 
 
-export const mainListItems = (
-  <div>
-    <ListItem button>
-      <ListItemIcon>
-        <DashboardIcon />
-      </ListItemIcon>
-      <ListItemText primary="Início" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <PeopleIcon />
-      </ListItemIcon>
-      <ListItemText primary="Usuários" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <SchoolIcon />
-      </ListItemIcon>
-      <ListItemText primary="Escolas" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <DirectionsBusIcon />
-      </ListItemIcon>
-      <ListItemText primary="Veículos" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <BarChartIcon />
-      </ListItemIcon>
-      <ListItemText primary="Relatórios" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <SettingsIcon />
-      </ListItemIcon>
-      <ListItemText primary="Configurações" />
-    </ListItem>
-  </div>
-);
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import Collapse from '@material-ui/core/Collapse';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import SendIcon from '@material-ui/icons/Send';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import StarBorder from '@material-ui/icons/StarBorder';
 
-export const secondaryListItems = (
-  <div>
-    <ListSubheader inset>Saved reports</ListSubheader>
-    <ListItem button>
-      <ListItemIcon>
-        <AssignmentIcon />
-      </ListItemIcon>
-      <ListItemText primary="Current month" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <AssignmentIcon />
-      </ListItemIcon>
-      <ListItemText primary="Last quarter" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <AssignmentIcon />
-      </ListItemIcon>
-      <ListItemText primary="Year-end sale" />
-    </ListItem>
-  </div>
-);
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+  nested: {
+    paddingLeft: theme.spacing(4),
+  },
+}));
+
+export default function NestedList() {
+  const classes = useStyles();
+  const [openRegister, setOpenRegister] = React.useState(false);
+  const [openList, setOpenList] = React.useState(false);
+
+  const handleClickRegister = () => {
+    setOpenRegister(!openRegister);
+  };
+
+  const handleClickList = () => {
+    setOpenList(!openList);
+  };
+
+  return (
+    <List
+      component="nav"
+      aria-labelledby="nested-list-subheader"
+      subheader={
+        <ListSubheader component="div" id="nested-list-subheader">
+          {/* Nested List Items */}
+        </ListSubheader>
+      }
+      className={classes.root}
+    >
+      {/* <ListItem button>
+        <ListItemIcon>
+          <SendIcon />
+        </ListItemIcon>
+        <ListItemText primary="Cadastro" />
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon>
+          <DraftsIcon />
+        </ListItemIcon>
+        <ListItemText primary="Drafts" />
+      </ListItem> */}
+      <ListItem button onClick={handleClickRegister}>
+        <ListItemIcon>
+          <InboxIcon />
+        </ListItemIcon>
+        <ListItemText primary="Cadastro" />
+        {openRegister ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={openRegister} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          <ListItem button className={classes.nested}>
+            <ListItemIcon>
+              <StarBorder />
+            </ListItemIcon>
+            <ListItemText primary="Starred" />
+          </ListItem>
+        </List>
+      </Collapse>
+
+      <ListItem button onClick={handleClickList}>
+        <ListItemIcon>
+          <InboxIcon />
+        </ListItemIcon>
+        <ListItemText primary="Listar" />
+        {openList ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={openList} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          <ListItem button className={classes.nested}>
+            <ListItemIcon>
+              <StarBorder />
+            </ListItemIcon>
+            <ListItemText primary="Starred" />
+          </ListItem>
+        </List>
+      </Collapse>
+    </List>
+  );
+}
+
+
+
+// export const mainListItems = (
+//   <div>
+//     <ListItem button>
+//       <ListItemIcon>
+//         <DashboardIcon />
+//       </ListItemIcon>
+//       <ListItemText primary="Início" />
+//     </ListItem>
+//     <ListItem button>
+//       <ListItemIcon>
+//         <PeopleIcon />
+//       </ListItemIcon>
+//       <ListItemText primary="Usuários" />
+//     </ListItem>
+//     <ListItem button>
+//       <ListItemIcon>
+//         <SchoolIcon />
+//       </ListItemIcon>
+//       <ListItemText primary="Escolas" />
+//     </ListItem>
+//     <ListItem button>
+//       <ListItemIcon>
+//         <DirectionsBusIcon />
+//       </ListItemIcon>
+//       <ListItemText primary="Veículos" />
+//     </ListItem>
+//     <ListItem button>
+//       <ListItemIcon>
+//         <BarChartIcon />
+//       </ListItemIcon>
+//       <ListItemText primary="Relatórios" />
+//     </ListItem>
+//     <ListItem button>
+//       <ListItemIcon>
+//         <SettingsIcon />
+//       </ListItemIcon>
+//       <ListItemText primary="Configurações" />
+//     </ListItem>
+//   </div>
+// );
+
+// export const secondaryListItems = (
+//   <div>
+//     <ListSubheader inset>Saved reports</ListSubheader>
+//     <ListItem button>
+//       <ListItemIcon>
+//         <AssignmentIcon />
+//       </ListItemIcon>
+//       <ListItemText primary="Current month" />
+//     </ListItem>
+//     <ListItem button>
+//       <ListItemIcon>
+//         <AssignmentIcon />
+//       </ListItemIcon>
+//       <ListItemText primary="Last quarter" />
+//     </ListItem>
+//     <ListItem button>
+//       <ListItemIcon>
+//         <AssignmentIcon />
+//       </ListItemIcon>
+//       <ListItemText primary="Year-end sale" />
+//     </ListItem>
+//   </div>
+// );
